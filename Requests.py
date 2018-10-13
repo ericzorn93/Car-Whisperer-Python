0d0c90c8583f292069605cf6329b411c543f07b8
Eric Zorn <ericzorn@Erics-MBP-2018.home>
2018-10-13 13:08:20 -0400

    Initial Commit and Request

---
+++ Requests.py
@@ -0,0 +1,98 @@
+import requests
+from prettytable import PrettyTable
+import json
+
+
+class MakeRequest(object):
+
+    def __init__(self, mb_url, lexus_url):
+        self.mb_api = mb_url
+        self.lexus_api = lexus_url
+        self.table = PrettyTable()
+
+        # Original data strings if pleased
+        self.mb_string_data = ""
+        self.lexus_string_data = ""
+
+    def get_mb_data(self):
+        """
+        Reaches out to the Mercedes Benz API for desired search query
+        :return: data
+        """
+        response = requests.get(self.mb_api)
+        data = response.text
+        self.mb_string_data += data
+        return data
+
+    def get_lexus_data(self):
+        """
+        Reaches out to the Lexus API for desired search query
+        :return: data
+        """
+        response = requests.get(self.lexus_api)
+        data = response.text
+        self.lexus_string_data += data
+        return data
+
+    def __create_table(self):
+        self.table.field_names = [
+            "Brand",
+            "Model",
+            "Year",
+            "Mileage",
+            "price",
+            "Vin Number",
+            "Exterior Color",
+            "Interior Color",
+            "Dealer City",
+            "Dealer State",
+            "Dealer Zip Code",
+            "Dealer Phone Number",
+            "Inventory URL"
+        ]
+
+        mb_parsed_data = json.loads(self.get_mb_data())['models']
+        lexus_parsed_data = json.loads(self.get_lexus_data())['data']['docs']
+
+        for car in mb_parsed_data:
+            self.table.add_row([
+                "Mercedes Benz",
+                car['vehicles']['records'][0]['modelName'],
+                car['vehicles']['records'][0]['year'],
+                car['vehicles']['records'][0]['mileage'],
+                car['vehicles']['records'][0]['dsrp'],
+                car['vehicles']['records'][0]['vin'],
+                car['vehicles']['records'][0]['exteriorColorDesc'],
+                car['vehicles']['records'][0]['interiorColorDesc'],
+                car['vehicles']['records'][0]['dealer']['city'],
+                car['vehicles']['records'][0]['dealer']['state'],
+                car['vehicles']['records'][0]['dealer']['zip'],
+                car['vehicles']['records'][0]['dealer']['phone'],
+                car['vehicles']['records'][0]['dealer']['url'],
+            ])
+
+        for car in lexus_parsed_data:
+            self.table.add_row([
+                "Lexus",
+                car['overview']['modelname'],
+                car['overview']['year'],
+                car['overview']['miles'],
+                car['overview']['lotPrice'],
+                car['overview']['vin'],
+                car['color']['exteriorcolorname'],
+                car['color']['interiorcolorname'],
+                car['dealerInfo']['address']['city'],
+                car['dealerInfo']['address']['state'],
+                car['dealerInfo']['address']['zipCode'],
+                car['dealerInfo']['phone'],
+                car['inventoryData']['inventoryUrl']['vehicle'][0],
+            ])
+
+        print(self.table)
+
+        return self.table
+
+    def output_table(self):
+        with open('auto_info.txt', 'w') as file:
+            file.write(str(self.__create_table()))
+            file.close()
---
+++ data_aggregator.py
@@ -0,0 +1,35 @@
+from Requests import MakeRequest
+import threading
+
+# Set API URl end points
+mb_url = "https://www.mbusa.com/mercedes/json/cpo/inventory/vehicles/search?model=E350W4&count=4&start=0&sortBy=distance&zip=07675&distance=50&minYear=2016&maxYear=2018&minPrice=3000&maxPrice=38000&minMileage=0&maxMileage=10000&callSeq=1&_=1538969508535"
+lexus_url = "https://www.lexus.com/rest/inventorySearch?year=2016,2017&model=ES&minMileage=0&maxMileage=10000&minPrice=0&maxPrice=38000&zip=07675&sort=l&radius=25&limit=12&offset=0"
+
+
+# Instances of Make Request Class
+first_request = MakeRequest(mb_url, lexus_url)
+
+
+def setInterval(func, time):
+    """
+    :param func: callback function
+    :param time: set amount of seconds
+    :return: Function Call
+    """
+    e = threading.Event()
+
+    while not e.wait(time):
+        func()
+
+
+def make_library_request():
+    """
+    Creates Table and Outputs Table
+    :return: None
+    """
+    first_request.output_table()
+    return None
+
+
+if __name__ == "__main__":
+    make_library_request()
