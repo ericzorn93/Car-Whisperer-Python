eafd72c02accb048f3ff45825800424ed7049ce5
Eric Zorn <ericzorn@Erics-MBP-2018.home>
2018-10-14 13:59:49 -0400

    Add C Class and E Class

--- Requests.py
+++ Requests.py
@@ -5,8 +5,9 @@ import json
 
 class MakeRequest(object):
 
-    def __init__(self, mb_url, lexus_url):
+    def __init__(self, mb_url, mb_c_class_url, lexus_url):
         self.mb_api = mb_url
+        self.mb_c_api = mb_c_class_url
         self.lexus_api = lexus_url
         self.table = PrettyTable()
 
@@ -20,6 +21,16 @@ class MakeRequest(object):
         :return: data
         """
         response = requests.get(self.mb_api)
+        data = response.text
+        self.mb_string_data += data
+        return data
+
+    def get_mb_c_class_data(self):
+        """
+        Reaches out to the Mercedes Benz API for desired search query
+        :return: data
+        """
+        response = requests.get(self.mb_c_api)
         data = response.text
         self.mb_string_data += data
         return data
@@ -52,9 +63,27 @@ class MakeRequest(object):
         ]
 
         mb_parsed_data = json.loads(self.get_mb_data())['models']
+        mb_c_parsed_data = json.loads(self.get_mb_c_class_data())['models']
         lexus_parsed_data = json.loads(self.get_lexus_data())['data']['docs']
 
         for car in mb_parsed_data:
+            self.table.add_row([
+                "Mercedes Benz",
+                car['vehicles']['records'][0]['modelName'],
+                car['vehicles']['records'][0]['year'],
+                car['vehicles']['records'][0]['mileage'],
+                car['vehicles']['records'][0]['dsrp'],
+                car['vehicles']['records'][0]['vin'],
+                car['vehicles']['records'][0]['exteriorColorDesc'],
+                car['vehicles']['records'][0]['interiorColorDesc'],
+                car['vehicles']['records'][0]['dealer']['city'],
+                car['vehicles']['records'][0]['dealer']['state'],
+                car['vehicles']['records'][0]['dealer']['zip'],
+                car['vehicles']['records'][0]['dealer']['phone'],
+                car['vehicles']['records'][0]['dealer']['url'],
+            ])
+
+        for car in mb_c_parsed_data:
             self.table.add_row([
                 "Mercedes Benz",
                 car['vehicles']['records'][0]['modelName'],
