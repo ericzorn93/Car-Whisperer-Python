c0c964899e4d37370791af08776fcc483b24c048
Eric Zorn <ericzorn@Erics-MBP-2018.home>
2018-10-14 13:13:09 -0400

    Add Updated MB URL

--- data_aggregator.py
+++ data_aggregator.py
@@ -3,11 +3,12 @@ import threading
 
 # Set API URl end points
 mb_url = "https://www.mbusa.com/mercedes/json/cpo/inventory/vehicles/search?model=E350W4&count=4&start=0&sortBy=distance&zip=07675&distance=50&minYear=2016&maxYear=2018&minPrice=3000&maxPrice=38000&minMileage=0&maxMileage=10000&callSeq=1&_=1538969508535"
+updated_mb_url = "https://www.mbusa.com/mercedes/json/cpo/inventory/vehicles/search?model=E350W4&count=4&start=0&sortBy=distance&zip=07675&distance=25&minYear=2016&maxYear=2018&minPrice=3000&maxPrice=38000&minMileage=0&maxMileage=11000&callSeq=1&_=1539534577948"
 lexus_url = "https://www.lexus.com/rest/inventorySearch?year=2016,2017&model=ES&minMileage=0&maxMileage=10000&minPrice=0&maxPrice=38000&zip=07675&sort=l&radius=25&limit=12&offset=0"
 
 
 # Instances of Make Request Class
-first_request = MakeRequest(mb_url, lexus_url)
+first_request = MakeRequest(updated_mb_url, lexus_url)
 
 
 def setInterval(func, time):
